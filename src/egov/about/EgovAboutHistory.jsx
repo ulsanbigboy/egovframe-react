db836c85021d684d4135f7c7f60e9f5d35ad3035
울산빅보이 <103149528+ulsanbigboy@users.noreply.github.com>
2022-06-11 17:49:02 +0900

    설명추가

--- src/egov/about/EgovAboutHistory.jsx
+++ src/egov/about/EgovAboutHistory.jsx
@@ -1,3 +1,10 @@
+{/* A JSX comment */}
+{/* A JSX comment */}
+{/* A JSX comment */}
+{/* A JSX comment */}
+{/* A JSX comment */}
+{/* A JSX comment */}
+{/* A JSX comment */}
 import React from 'react';
 import { Link } from 'react-router-dom';
 
